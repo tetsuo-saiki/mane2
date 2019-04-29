de0453424ef636896df7bc978812e26528c115cc
tetsuo <t.saiki@cellpromote.co.jp>
2019-04-30 00:06:29 +0900

    index対象のカラム量が767バイトを超えると出るエラーへの対処

--- db/migrate/20190301053905_devise_create_users.rb
+++ db/migrate/20190301053905_devise_create_users.rb
@@ -4,11 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: "", limit: 191
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string   :reset_password_token, limit: 191
       t.datetime :reset_password_sent_at
 
       ## Rememberable
